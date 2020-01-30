c9bd4f0800a1095e94229b3e8a1594bd65c734b4
f45d07 <f45d07@users.noreply.github.com>
2020-01-30 05:18:48 +0300

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { exec } = require("child_process");
+const { execSync } = require("child_process");
 const app = express();
 const port = process.env.PORT || 8088;
 
@@ -11,16 +11,9 @@ app.post('/hook', (req, res) => {
     res.send('Got!');
     var git_name = req.body.repository.name;
     var git_url = req.body.repository.clone_url;
-    exec('pkill -f "node '+git_name+'/server.js"; rm -r /root/'+git_name+'; git clone '+git_url+'; node '+git_name+'/server.js /root/;', (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
+    execSync('pkill -f "node '+git_name+'/server.js"');
+    execSync('rm -rf /root/'+git_name);
+    execSync('git clone '+git_url+' /root/'+git_name);
+    execSync('node /root/'+git_name+'/server.js');
     console.log('Cloned!');
 });
