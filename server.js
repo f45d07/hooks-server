33c2623b326fab46f3ab55ac304f8b56d9f39f35
f45d07 <f45d07@users.noreply.github.com>
2020-01-30 03:53:42 +0300

    server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8088;
 
+app.use(express.json());
+
 app.listen(port, () => console.log(`Running on port ${port}`));
 
 app.post('/hook', (req, res) => {
     res.send('Got!');
-    console.log(req);
+    console.log(req.body);
 });
