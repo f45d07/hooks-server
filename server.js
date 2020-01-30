a4fa5e3d785196e45d0c85999b8db90e77704bdc
f45d07 <f45d07@users.noreply.github.com>
2020-01-30 05:40:52 +0300

    server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.post('/hook', (req, res) => {
     res.send('Got!');
     var git_name = req.body.repository.name;
     var git_url = req.body.repository.clone_url;
-    execSync('pkill -f "node '+git_name+'/server.js"', function (error, stdout, stderr) {
+    execSync('if pgrep "node '+git_name+'/server.js"; then pkill -f "node '+git_name+'/server.js"; fi', function (error, stdout, stderr) {
         if(error) console.log(error);
     });
     execSync('rm -rf /root/'+git_name, function (error, stdout, stderr) {
