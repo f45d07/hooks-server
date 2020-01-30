1ac1fd0f3926e9e0587a1fb66c004da7e50dbc8f
f45d07 <f45d07@users.noreply.github.com>
2020-01-30 04:59:25 +0300

    server.js

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.post('/hook', (req, res) => {
     res.send('Got!');
     var git_name = req.body.repository.name;
     var git_url = req.body.repository.clone_url;
-    exec('pkill -f "node '+git_name+'/server.js"; rm -r ~/'+git_name+'; git clone '+git_url+'; node '+git_name+'/server.js;');
+    exec('pkill -f "node '+git_name+'/server.js"; rm -r ~/'+git_name+'; git clone '+git_url+'; node '+git_name+'/server.js;', (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
     console.log('Cloned!');
 });
