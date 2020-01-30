132a351aa28a8c34833c83d5a92ef3ebf94e266a
f45d07 <f45d07@users.noreply.github.com>
2020-01-30 03:37:06 +0300

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const port = process.env.PORT || 8088;
 
 app.listen(port, () => console.log(`Running on port ${port}`));
 
-app.get('/hook', (req, res) => {
+app.post('/hook', (req, res) => {
+    res.send('Got!');
     console.log(req);
     console.log(res);
 });
